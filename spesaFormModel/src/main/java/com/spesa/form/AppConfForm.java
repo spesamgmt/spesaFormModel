8f267d6fa6e428d08aea28b34a65a54250b8e1be
Bhavesh <Bhavesh@Bhavesh-P>
2018-05-20 22:43:17 +0530

    Initial commit

---
+++ spesaFormModel/src/main/java/com/spesa/form/AppConfForm.java
@@ -0,0 +1,16 @@
+package com.spesa.form;
+
+import java.io.Serializable;
+
+public class AppConfForm implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+
+}
